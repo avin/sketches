e4cc9c15aa3acc429696f9b1045ba466df95e105
Avin <carcinogen75@gmail.com>
2019-02-28 11:17:15 +0300

    optimize 018_hair

--- src/018_hair.js
+++ src/018_hair.js
@@ -18,7 +18,13 @@ const sketch = ({ width, height }) => {
     for (let y = 0; y < 1; y += step) {
         const row = [];
         for (let x = 0; x < 1; x += step) {
-            row.push([x + random.range(-step, step), y + random.range(-step, step)]);
+            row.push([
+                x + random.range(-step, step),
+                y + random.range(-step, step),
+                random.range(0.7, 1), // for hair length
+                random.range(0.9, 1), // for rotation
+                random.value(), // for hair width
+            ]);
         }
         matrix.push(row);
     }
@@ -28,24 +34,19 @@ const sketch = ({ width, height }) => {
         context.fillStyle = 'hsl(0, 0%, 98%)';
         context.fillRect(0, 0, width, height);
 
-        context.lineWidth = width * 0.0001;
-
-        matrix.forEach(row => {
-            row.forEach(p => {
+        for (const row of matrix) {
+            for (const p of row) {
                 const rI = random.noise2D(p[0] + time / 5, p[1], 1.5) / 2 + 0.5;
-                context.save();
+                const rotateF = rI * Math.PI * 2 * p[3];
+                const lineWidth = width * 0.05 * p[2];
                 context.beginPath();
-                context.translate(x(p[0]), y(p[1]));
-                context.rotate(rI * Math.PI * 2);
-                context.strokeStyle = `hsl(0,0%,${20 + rI * 20}%)`;
+                context.lineWidth = (20 + rI * 20) * width * 0.000005 * (p[4] > 0.5 ? 1 : 1.18);
 
-                context.moveTo(0, 0);
-                context.lineTo(width * 0.05 * random.range(0.8, 1), 0);
-
+                context.moveTo(x(p[0]), y(p[1]));
+                context.lineTo(x(p[0]) + Math.cos(rotateF) * lineWidth, y(p[1]) + Math.sin(rotateF) * lineWidth);
                 context.stroke();
-                context.restore();
-            });
-        });
+            }
+        }
     };
 };
 
---
+++ src/examples/fps_show.js
@@ -0,0 +1,23 @@
+import canvasSketch from 'canvas-sketch';
+import { getPointCoordsByAngleAndDistance, isCircleInCircle, twoCirclesIntersection } from './lib/geometry';
+import Stats from 'stats.js';
+
+const settings = {
+    dimensions: [1024, 1024],
+    animate: true,
+};
+
+const sketch = async ({ width, height }) => {
+    const stats = new Stats();
+    document.body.appendChild(stats.dom);
+
+    return ({ context, width, height, time, playhead }) => {
+        stats.begin();
+
+        // ... DRAW CODE ...
+
+        stats.end();
+    };
+};
+
+canvasSketch(sketch, settings);
