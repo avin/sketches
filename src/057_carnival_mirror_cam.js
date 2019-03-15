99bf17fe5fd53052c140af3c994ef4bdd7d9f467
Avin <carcinogen75@gmail.com>
2019-03-15 10:28:51 +0300

    controls for 057_carnival_mirror_cam

--- src/057_carnival_mirror_cam.js
+++ src/057_carnival_mirror_cam.js
@@ -1,4 +1,5 @@
 import canvasSketch from 'canvas-sketch';
+import * as dat from 'dat.gui';
 import { setPixel } from './lib/ctx';
 import WebCam from './lib/webcam';
 
@@ -9,7 +10,18 @@ const settings = {
     animate: true,
 };
 
-const sketch = async () => {
+const sketch = async ( ) => {
+    const params = {
+        strength: 10,
+        step: 10,
+        offset: 0,
+    };
+
+    const gui = new dat.GUI();
+    gui.add(params, 'strength', 0, 50);
+    gui.add(params, 'step', 0, 50);
+    gui.add(params, 'offset', -Math.PI, Math.PI);
+
     return ({ context, width, height }) => {
         context.fillStyle = 'hsl(0, 0%, 98%)';
         context.fillRect(0, 0, width, height);
@@ -23,8 +35,8 @@ const sketch = async () => {
 
         for (let y = 1; y < height; y += 1) {
             for (let x = 0; x < width; x += 1) {
-                const iy = Math.floor(camImageData.height * y / height);
-                const ix = Math.floor(camImageData.width * x / width);
+                const iy = Math.floor((camImageData.height * y) / height);
+                const ix = Math.floor((camImageData.width * x) / width);
 
                 const pixelIdx = camImageData.width * 4 * iy + 4 * ix;
 
@@ -32,7 +44,7 @@ const sketch = async () => {
                 const g = camImageData.data[pixelIdx + 1];
                 const b = camImageData.data[pixelIdx + 2];
 
-                setPixel(imageData, x, y + ~~(Math.cos(x / 10) * 10), r, g, b);
+                setPixel(imageData, x, y + ~~(Math.cos(x / params.step + params.offset) * params.strength), r, g, b);
             }
         }
 
