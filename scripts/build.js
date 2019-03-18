9f11959010be247ceb3abae7b1ea82ce9e4f5f08
Avin <carcinogen75@gmail.com>
2019-03-18 17:28:27 +0300

    add quality to image resizer

--- scripts/build.js
+++ scripts/build.js
@@ -59,12 +59,16 @@ images.forEach(image => {
     const imageSrc = `${previewFolder}${image}`;
     sharp(imageSrc)
         .resize(200, 200)
-        .quality(100)
+        .jpeg({
+            quality: 100,
+        })
         .toFile(`${buildFolder}preview/${image}`);
 
     sharp(imageSrc)
         .resize(30, 30)
-        .quality(100)
+        .jpeg({
+            quality: 100,
+        })
         .toFile(`${buildFolder}preview/small/${image}`);
 });
 
