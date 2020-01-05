6e10863e89e8c017480da57b8787ed0f1195484c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-05 12:58:15 +0000

    Deploy avin/sketches to github.com/avin/sketches.git:gh-pages

---
+++ precache-manifest.1593f5462bd99584911a0e6eb6027220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443c9e10a1ef45a9aec3997954c8fc0a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7bc7d37ad4fdcd50ade",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "44a3dd01109e0b4b6183",
+    "url": "./static/css/main.9bb80d51.chunk.css"
+  },
+  {
+    "revision": "f7bc7d37ad4fdcd50ade",
+    "url": "./static/js/2.518bb4a0.chunk.js"
+  },
+  {
+    "revision": "44a3dd01109e0b4b6183",
+    "url": "./static/js/main.3830813b.chunk.js"
+  },
+  {
+    "revision": "6eca7b581e0b2311acc8",
+    "url": "./static/js/runtime-main.70e4b77e.js"
+  }
+]);
